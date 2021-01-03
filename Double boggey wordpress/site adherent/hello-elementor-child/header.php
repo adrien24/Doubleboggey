76a740018c8a9d2f1fc94d5a32e8ca7bbc2f3054
adrien24 <59483650+adrien24@users.noreply.github.com>
2021-01-03 11:52:09 +0100

    Add files via upload

---
+++ Double boggey wordpress/site adherent/hello-elementor-child/header.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * This is the template that displays all of the <head> section, opens the <body> tag and adds the site's header.
+ *
+ * @package HelloElementor
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<?php $viewport_content = apply_filters( 'hello_elementor_viewport_content', 'width=device-width, initial-scale=1' ); ?>
+	<meta name="viewport" content="<?php echo esc_attr( $viewport_content ); ?>">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<body <?php body_class(); ?>>
+
+<header class="navbar navbar-expand-md navbar-dark">
+
+    <a href="https://doubleboggey2.adrienbouteiller.com/" class="navbar-brand ml-2 mt-2">
+       <img src="https://adherent.adrienbouteiller.com/wp-content/uploads/2021/01/logo-blancvv-8.png" alt="logo"/>
+
+    </a>
+
+    <button class="navbar-toggler " data-toggle="collapse" data-target="#navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navigation">
+
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item"><a class="nav-link active" href="https://doubleboggey2.adrienbouteiller.com/fil-dactualites/">Fil d'actualités</a></li>
+            <li class="nav-item"><a class="nav-link active" href="https://adherent.adrienbouteiller.com/compte-dadherent/">Profil</a></li>
+            <li class="nav-item"><a class="nav-link active" href="https://adherent.adrienbouteiller.com/agenda/">Agenda</a></li>
+            <li class="nav-item"><a class="nav-link active" href="https://adherent.adrienbouteiller.com/boutique-2/">Boutique</a></li>
+            <li class="nav-item"><a class="nav-link active" href="https://adherent.adrienbouteiller.com/"><img src="https://doubleboggey2.adrienbouteiller.com/wp-content/uploads/2020/11/icon_maison.png" alt="logo_maison"></a></li>
+        </ul>
+
+    </div>
+
+
+</header>
+
+<?php
+hello_elementor_body_open();
+
+if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'header' ) ) {
+	get_template_part( 'template-parts/header' );
+}
---
+++ Double boggey wordpress/site vitrine/hello-elementor-child/footer.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the body & html closing tags.
+ *
+ * @package HelloElementor
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'footer' ) ) {
+	get_template_part( 'template-parts/footer' );
+}
+?>
+
+<?php wp_footer();
+?>
+
+<footer class="row">
+
+
+
+
+
+
+
+
+
+    <div class="logo col-12 d-flex justify-content-center h-25" style="width: 70%;"><img src="http://doubleboggey2.adrienbouteiller.com/wp-content/uploads/2021/01/Fichier-1.png" alt="logo"></div>
+    <div class="col-12 d-flex justify-content-center"><p style="border-bottom: solid 1px black; padding-left: 27%; padding-right: 25%; padding-bottom: 2%">Retrouver nous aussi sur :<a href="https://www.facebook.com/DoubleBoggey"><img src="https://doubleboggey2.adrienbouteiller.com/wp-content/uploads/2020/11/facebook.png" alt="facebook" style="width: 15%; margin-left: 2%"></a> </p></div>
+    <a href="http://doubleboggey2.adrienbouteiller.com/politique-de-confidentialite/" class="col-lg-6 col-md-7   d-flex justify-content-center text-primary" style="text-align: center">Mention légales</a>
+    <a href="https://doubleboggey2.adrienbouteiller.com/FAQ" class="col-lg-6 col-md-7  d-flex justify-content-center text-primary" style=" text-align: center">FAQ</a>
+
+
+</footer>
+
+</body>
+</html>
